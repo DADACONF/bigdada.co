fd8c6fb820125085d5a6cc7cd259321414b76236
Siegfried Bilstein <sbilstein@gmail.com>
2014-05-01 22:31:03 -0700

    Updated functions to use fillRect, intervals

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,24 +1,6 @@
 require.config();
 
 require([], function() {
-	function setPixel(imageData, x, y, color) {	
-		var index = (x + y * imageData.width) * 4;
-		if(color.length < 4) {
-			throw "Color array requires 4 values: [r, g, b, a]!"
-		}
-		imageData.data[index + 0] = color[0];
-		imageData.data[index + 1] = color[1];
-		imageData.data[index + 2] = color[2];
-		imageData.data[index + 3] = color[3];
-	}
-
-	function randColorArray() {
-		var r = Math.random() * 256 | 0;
-    var g = Math.random() * 256 | 0;
-    var b = Math.random() * 256 | 0;
-		return [r,g,b,255];
-	}
-
 	function randColor() {
 		var r = Math.random() * 256 | 0;
     var g = Math.random() * 256 | 0;
@@ -26,11 +8,9 @@ require([], function() {
     return 'rgba(' + r + "," + g + "," + b + "," + 255.0 + ")";
   }
 
-
 	function washUpAndWashDown(drawingContext, cWidth, cHeight, stripeWidth, color) {
 		var numStripes =  cWidth / stripeWidth;
 		washDown(drawingContext, 0,  stripeWidth, 0, cWidth, cHeight, 60, randColor());
-		// washDown(drawingContext, stripeWidth, stripeWidth, cHeight, cHeight, - 10, randColor());
 	}
 
 	function washDown(drawingContext, x, stripeWidth, y, cWidth, cHeight, increment, color) {
@@ -70,33 +50,26 @@ require([], function() {
 		}
 	}
 
-	function drawBitLayer(imageData, layer, layers, color) {		
-			// print top
-			for(var x = layers - layer; x < layers + layer - 1; x++) {
-				setPixel(imageData, x, layers - layer, color);	
-  		};
-  		// print right
-  		for(var y = layers - layer; y < layers + layer; y++) {
-				setPixel(imageData, layers + layer - 1, y, color);	
-  		}
-  		// print bottom
-  		for(var x = layers + layer - 1; x >= layers - layer; x--) {
-				setPixel(imageData, x, layers + layer - 1, color);	  			
-  		}
-  	 	// print left
-			for(var y = layers + layer - 1; y >= layers - layer; y--) {
-			 	setPixel(imageData, layers - layer, y, color);	  			
-  	 	}  		
+	function drawLayer(drawingContext, layer, layers, color) {		
+			drawingContext.fillStyle = color;
+			var x0 =  layers - layer;
+			var x1 = layers + layer;
+			var y0 = layers - layer;
+			var y1 = layers + layer;
+			var length = layer * 2;
+			drawingContext.fillRect(x0, y0, length, 1);
+			drawingContext.fillRect(x0, y1, length + 1, 1);		
+			drawingContext.fillRect(x0, y0, 1, length);
+			drawingContext.fillRect(x1, y0, 1, length);
 	}
 
-	function drawSpirals(imageData, canvas2DContext, currentLayer, totalLayers) {
-		if(currentLayer > totalLayers) {
-			currentLayer = 0;
-		}
-		drawBitLayer(imageData, currentLayer, totalLayers, randColorArray());
-		setTimeout(function(){
-					drawSpirals(imageData, canvas2DContext, currentLayer + 1, totalLayers);
+	function drawSpirals(imageData, canvas2DContext, totalLayers) {
+		var currentLayer = 0;
+		var intervalId = setInterval(function(){
+			drawLayer(imageData, currentLayer, totalLayers, randColor());
+			currentLayer = (currentLayer + 1)	% (totalLayers + 1);
 		}, 5);
+		return intervalId;
 	}
 
 	$(document).ready(function() {
@@ -106,10 +79,10 @@ require([], function() {
   	var xLayers = width / 2;
   	var height = canvas.height;
   	var imageData = canvas2DContext.createImageData(width, height);
-  	washUpAndWashDown(canvas2DContext, width, height, 11, randColor());
-  	// drawSpirals(imageData, canvas2DContext, 0, xLayers);
-  	// setInterval(function(){
-  		// canvas2DContext.putImageData(imageData, 0, 0);
-  	// }, 50);
+  	// washUpAndWashDown(canvas2DContext, width, height, 11, randColor());
+  	var spiralInterval = drawSpirals(canvas2DContext, canvas2DContext, xLayers);
+  	$("#spiral-control").click(function() {
+	  	clearInterval(spiralInterval);
+	  });
 	});
 });
